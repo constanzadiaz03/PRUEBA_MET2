821ef718f536a5582795e7c572eae6bf5b6fa9e0
Constanza <constanza.romina2003@gmail.com>
2025-11-21 20:11:10 -0300

    AJUSTES BACKEND

--- BACKEND/src/app.module.ts
+++ BACKEND/src/app.module.ts
@@ -2,10 +2,17 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/tu_db'),
+    ConfigModule.forRoot({
+      isGlobal: true,              // HACE DISPONIBLE ConfigService EN TODA LA APP
+      envFilePath: '.env',         // ARCHIVO .env EN LA RAÍZ DEL BACKEND
+    }),
+
+    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/tu_db'),
+
     AuthModule,
     UsersModule,
   ],
--- BACKEND/src/auth/auth.module.ts
+++ BACKEND/src/auth/auth.module.ts
@@ -1,29 +1,21 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { ConfigModule } from '@nestjs/config';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
-    UsersModule,
+    ConfigModule,            
     PassportModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
-        signOptions: {
-          expiresIn: configService.get<string>('JWT_EXPIRES_IN', '1d'),
-        },
-      }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
     }),
+    UsersModule
   ],
-  controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
-  exports: [AuthService],
+  providers: [JwtStrategy],
+  exports: [JwtStrategy],
 })
 export class AuthModule {}
